8a25cacaa00abe71824dd180ece0062d146b2e05
JJ Guerrette <jonathan.guerrette@noaa.gov>
2016-09-08 14:08:49 -0700

    Modified the Gram-Schmidt orthog in SVD to improve numerical precision
    Also added oversampling to RSVD5.6

--- var/da/da_randomisation/da_randomisation.f90
+++ var/da/da_randomisation/da_randomisation.f90
@@ -11,7 +11,7 @@ module da_randomisation
    use da_control, only : svd_stage, ensmember, ensdim_svd, svd_outer, &
        myproc, filename_len, test_dm_exact, rootproc, cv_size_domain, &
        stdout, trace_use, svd_amat_type, svd_symm_type, adapt_svd, &
-       num_ob_indexes, read_omega, ierr, comm, &
+       num_ob_indexes, read_omega, svd_p, ierr, comm, &
 #if (WRF_CHEM == 1)
        chem_surf, chem_acft, &
 #endif
